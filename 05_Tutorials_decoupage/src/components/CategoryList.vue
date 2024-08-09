1c504dc7a88a3ad0b1a72d2e9ceca3edd2b31d02
sauvageb <sauvageboris.pro@gmail.com>
2024-08-09 09:00:29 +0200

    exercice 5

---
+++ 05_Tutorials_decoupage/src/components/CategoryList.vue
@@ -0,0 +1,30 @@
+<script setup>
+const categories = [
+  'Java',
+  'Javascript',
+  'Typescript',
+  'VueJs',
+  'C#',
+  'Angular',
+  'Ruby',
+  'Python',
+  'C++',
+  'Rust',
+  'VBA'
+];
+</script>
+
+<template>
+  <div class="container">
+    <h1>Catégories</h1>
+    <div class="col-6 mb-3">
+      <div class="row">
+        <div class="card mb-3" v-for="category in categories" :key="category">
+          <div class="card-body">
+            {{ category }}
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
