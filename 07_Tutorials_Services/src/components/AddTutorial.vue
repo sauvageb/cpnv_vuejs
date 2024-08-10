23a49e0711025eb015f25b1b1120d3dd6194064e
sauvageb <sauvageboris.pro@gmail.com>
2024-08-10 19:35:13 +0200

    exercice 7 + atelier ensemble ajout categorie

--- 07_Tutorials_Services/src/components/AddTutorial.vue
+++ 07_Tutorials_Services/src/components/AddTutorial.vue
@@ -1,39 +1,6 @@
 <template>
-  <div class="row justify-content-center">
-    <div class="col-lg-6 mb-5">
-      <h1>Nouveau tutorial</h1>
-
-      <div class="form-group mt-3">
-        <input type="text" name="title" class="form-control" placeholder="Titre" required>
-      </div>
-      <div class="form-group mt-3">
-        <input type="text" name="lead" class="form-control" placeholder="Description" required>
-      </div>
-      <div class="form-group mt-3">
-        <label for="category">Categorie</label>
-        <select class="form-select" id="category" name="category">
-          <option v-for="category in categories" :key="category.id">{{ category.name }}</option>
-        </select>
-      </div>
-
-      <div class="form-group mt-3">
-        <label for="content">Description</label>
-        <textarea class="form-control" id="content" name="content" rows="3" required></textarea>
-      </div>
-
-      <div class="form-group mt-3">
-        <label for="date">Date</label>
-        <input class="form-control" type="date" id="date" name="date" rows="3" required/>
-      </div>
-
-
-      <button type="submit" class="btn btn-primary mt-3">Ajouter</button>
-    </div>
-  </div>
+  <h1>Ajout d'un tutoriel</h1>
 </template>
 
 <script setup>
-import {ref} from "vue";
-
-const categories = ref([]);
 </script>
